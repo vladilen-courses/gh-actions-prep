a07e0f9d2cfaf4e5ab5794c1c2772054c6424738
Vladilen <mininvlad@gmail.com>
2022-12-20 14:50:32 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import { useState } from 'react'
+
+export default function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
+}
